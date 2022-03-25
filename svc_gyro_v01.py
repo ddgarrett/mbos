66dc866c730d9b365535644a67c3ef8f85bd8f87
ddgarrett <doug@anetsc.com>
2022-03-25 09:43:56 -0700

    Defer IMU Create Until First onFocus

--- svc_gyro_v01.py
+++ svc_gyro_v01.py
@@ -22,14 +22,16 @@ class ModuleService(Service):
         
         self.display_task = None
         
-        dev_addr = self.get_parm("device_addr",0)
-        self.imu = MPU6050(self.get_i2c(),device_addr=dev_addr)
-        
+        self.imu = None
         self.adj_x = self.get_parm("adj_x",0)
 
 
     async def gain_focus(self):
         await super().gain_focus()
+        
+        if self.imu == None:       
+            dev_addr = self.get_parm("device_addr",0)
+            self.imu = MPU6050(self.get_i2c(),device_addr=dev_addr)
         
         if self.display_task == None:
             self.display_task = uasyncio.create_task(self.display_data())
