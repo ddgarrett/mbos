c3044e723cba3022fe20a5654ab2fce0bd24e86d
ddgarrett <doug@anetsc.com>
2022-03-29 16:46:33 -0700

    Scan Both I2C Buses

--- i2c_test.py
+++ i2c_test.py
@@ -9,18 +9,33 @@ import utime
 
 """
 
-I2C_FREQUENCY = 100000
+I2C_FREQUENCY = 400_000
 
+i2c_parm = [
+    [0, 100_000, 20, 21],
+    [1, 400_000, 14, 15]  
+]
+
+"""
 CONTROLLER_I2C_DEVICE_ID = 0
 GPIO_CONTROLLER_SCL = 21
 GPIO_CONTROLLER_SDA = 20
+"""
 
+
+CONTROLLER_I2C_DEVICE_ID = 1
+GPIO_CONTROLLER_SCL = 15
+GPIO_CONTROLLER_SDA = 14
+
+
+"""
 i2c_controller = I2C(
     CONTROLLER_I2C_DEVICE_ID,
     scl=Pin(GPIO_CONTROLLER_SCL),
     sda=Pin(GPIO_CONTROLLER_SDA),
     freq=I2C_FREQUENCY,
-)   
+)
+"""
 
 
 def to_hex(value):
@@ -41,6 +56,15 @@ def format_hex(_object):
 # -----------------
 print('Scanning I2C Bus for Responders...')
 
+for p in i2c_parm:
+    i2c = I2C(p[0], freq=p[1], sda=Pin(p[2]), scl=Pin(p[3]))
+    print("\nscanning bus {}".format(p[0]))
+    resp = i2c.scan()
+    print("    Addresses found: {}".format(resp))
+
+    
+"""
+
 ticks_start = utime.ticks_us()
 
 resp_addr = i2c_controller.scan()
@@ -55,7 +79,9 @@ resp_addr = i2c_controller.scan()
 resp_addr = i2c_controller.scan()
 
 diff = (utime.ticks_us() - ticks_start)/10
+"""
 
+"""
 print('I2C Addresses of Responders found: ' , end='')
 print(resp_addr, end='')
 print(' in {0:,.0f} μsec'.format(diff))
@@ -68,11 +94,14 @@ print(' in {0:,.0f} μsec'.format(diff))
 # print('I2C Addresses of Responders found: ' + format_hex(resp_addr))
 print()
 
+
 # rtc_addr = 0x68
 
 # x = input("Year : ")
 
 # print("you said: " + x)
+
+"""
 
 
 
