8531e85f97d9e3d9e0ef17ce2a6cce156dc7ec08
ddgarrett <doug@anetsc.com>
2022-04-01 07:50:38 -0700

    Print Name from JSON When Booting

--- i2c_responder/mbos_boot.py
+++ i2c_responder/mbos_boot.py
@@ -14,6 +14,9 @@ with open("core_0_services.json") as f:
         parms = ujson.load(f)
 
 # start main
+if "name" in parms:
+    print("starting",parms["name"])
+          
 main_name = parms["main"]
 print("starting " + main_name)
 
--- mbos_boot.py
+++ mbos_boot.py
@@ -14,10 +14,14 @@ with open("core_0_services.json") as f:
         parms = ujson.load(f)
 
 # start main
+if "name" in parms:
+    print("starting",parms["name"])
+          
 main_name = parms["main"]
 print("starting " + main_name)
 
 main = __import__(main_name)
 uasyncio.run(main.main(parms))
     
+
 
