25a7a69b79333c27e34ca0bc149b28cb222c48eb
ddgarrett <doug@anetsc.com>
2022-04-10 08:31:31 -0700

    Generalize Remote Log for Any Responder Log

--- svc_test_i2c.py
+++ svc_test_i2c.py
@@ -17,6 +17,8 @@ class ModuleService(Service):
         super().__init__(svc_parms)
         
         self.send_cnt = 0
+        
+        self.send_to = self.get_parm("send_to","remote_log")
             
     async def gain_focus(self):
         await super().gain_focus()
@@ -27,12 +29,12 @@ class ModuleService(Service):
         controller = self.get_parm("i2c_controller",None)
         
         xmit = xmit_lcd.XmitLcd(fr=self.name).clear_screen()
-        xmit.set_msg("⏶ press any key" + "\n⏷ to start tests\n    sent: ")
+        xmit.set_msg("⏶ press any key" + "\n⏷ to send 10 xmit\np:       {}".format(self.send_to))
         await self.put_to_output_q(xmit)
 
         
         xmit = xmit_lcd.XmitLcd(fr=self.name)
-        xmit.set_cursor(10,2).set_msg(str(self.send_cnt))
+        xmit.set_cursor(2,2).set_msg(str(self.send_cnt))
         
         m = "s{} r{} x{} f{}".format(controller.send_cnt,
                 controller.rcv_cnt, controller.resend_cnt, controller.failed_cnt )
@@ -49,14 +51,14 @@ class ModuleService(Service):
             self.send_cnt += 1
                         
             xmit = xmit_lcd.XmitLcd(fr=self.name)
-            xmit.set_cursor(10,2).set_msg(str(self.send_cnt))
+            xmit.set_cursor(2,2).set_msg(str(self.send_cnt))
             await self.put_to_output_q(xmit)
 
             # send a quick burst of 10 messages
             # print("************************** svc_test_i2c: ")
             for i in range(10):
-                await self.send_msg("remote_log","test forwarded message!")
-                await uasyncio.sleep_ms(600)
+                await self.send_msg(self.send_to,"test forwarded message!")
+                await uasyncio.sleep_ms(500)
 
                 m = "s{} r{} x{} f{}".format(controller.send_cnt,
                     controller.rcv_cnt, controller.resend_cnt, controller.failed_cnt )
